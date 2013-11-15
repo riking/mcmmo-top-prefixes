75d7b7fbbdbdc110a812c58c4c19f9d38c732a39
riking <rikingcoding@gmail.com>
2013-11-15 15:24:45 -0800

    Initial commit - no code, only configs

---
+++ src/main/java/com/github/riking/mcmmotop/Plugin.java
@@ -0,0 +1,10 @@
+package com.github.riking.mcmmotop;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Plugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+}
